3636f6c5536da29bb31b0dca1459b53eb228d370
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2023-01-19 03:55:28 -0600

    Update Solución.php

--- LiskovSustitution/Solución.php
+++ LiskovSustitution/Solución.php
@@ -1,30 +1,34 @@
 <?php
 
-class AudioLibro{
 
-    public function minDuración(){
+//La solución es que podemos crear opciones con funciones únicas y parecidas, que sean especificas para no generalizar
+class AudioLibro{//creamos una opción solo para los audiolibros
+
+    public function minDuración(){//posee la función de conocer la duración en minutos
         //Código
     }
 
-    public function formatoAudio(){
+    public function formatoAudio(){//posee la función de conocer el formato
         //Código
  
     }
 }
 
-class LibroFísico{
+class LibroFísico{//opción solo para libros físicos
 
-    public function númPags(){
+    public function númPags(){//para saber número de págs.
         //Código
     }
 
-    public function tipoDePasta(){
+    public function tipoDePasta(){//para saber el tipo de pasta 
         //Código
     }
 }
 
+//Al agregar un audiolibro estaremos ocupando con la palabra 'extends' las características ya planteadas en la clase de audiolibro
 class AudioLibroLaIliada extends LibroFísico{
 
+    //Se vuelven a escribir dichas funciones
     public function númPags(){
         //Código
     }
