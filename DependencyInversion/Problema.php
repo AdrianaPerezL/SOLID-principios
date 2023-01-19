f291fcf86378e52ba7ec949f6cb5d8835a81ce36
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2023-01-19 04:13:46 -0600

    Update Problema.php

--- DependencyInversion/Problema.php
+++ DependencyInversion/Problema.php
@@ -1,22 +1,32 @@
 <?php
-class FormaGeometrica{
-    protected $area;
+
+//El principio de dependency inversion nos dice que las clases principales, o sea las opciones generales
+//No pueden depender de las específicas
+
+//Por ejemplo tenemos la opción de estudiantes, necesitamos que nos imprima el curso
+//Pero esta clase general retoma valores de una clase más pequeña que ella, es decir la clase Primero
+//Asi que esta clase depende de las mas pequeñas y tendremos que editarla constantemente.
+class Estudiante{
+    protected $curso;
   
+
 
     public function __construct()
     {
-        $this->area = new AreaTriangulo();
+        $this->curso = new Primero();
     }
 
 }
 
 
-class AreaTriangulo{
- //Código
+//soy una clase más pequeñita
+class Primero{
+ //Código de estudiante de primer curso
 }
 
-class AreaRectángulo{
- //Código
+//Yo también soy una clase pequeñita
+class Segundo{
+ //Código de estudiante de segundo curso
 }
 
 ?>
