f4d7a0e2064717995d208c0596a5587ce6f69475
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2023-01-19 04:06:02 -0600

    Update Problema.php

--- InterfaceSegregation/Problema.php
+++ InterfaceSegregation/Problema.php
@@ -1,4 +1,11 @@
 <?php 
+
+//El principio de InterfaceSegregation nos dice que al crear interfaces se y retomarlas en otro lugar 
+//se deben colocar todo el contenido que tenga la interface
+//Como vimos en el Liskov sustitution, la clase o opción que contiene tareas asignadas podemos tomar una tarea de esas
+//pero para las interfaces debemos tomar todas las tareas que se asignen
+
+//Por ejemplo hacemos una interface que contenga 3 tipos de menú de un restaurante
 interface Menús{
     public function menú1();
     public function menú2();
@@ -6,15 +13,16 @@ interface Menús{
 
 }
 
+//y el cliente solo nos pide el menú1
 class Cliente1 implements Menús{
     public function menú1();
-    public function menú2();
-    public function menú3();
+    //como estamos ocupando la interface menú y ahí estarán agregados 3 menús, en esta función se deberan agregar los otros 2
+    //Y si no se agregan el código esta incorrecto
 }
 
+//Lo mismo con un cliente dos 
 class Cliente2 implements Menús{
-    public function menú1();
-    public function menú2();
+
     public function menú3();
 }
 ?>
