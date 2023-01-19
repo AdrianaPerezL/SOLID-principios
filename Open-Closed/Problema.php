9609e39f723b5769776897d6ee4dad1b1eafd9ed
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2023-01-19 03:41:38 -0600

    Update Problema.php

--- Open-Closed/Problema.php
+++ Open-Closed/Problema.php
@@ -1,30 +1,32 @@
 <?php
-//El principio de Open-closed nos ayuda a modificar cierta parte del código sin afectar más clases o funciones
+//El principio de Open-closed nos ayuda a modificar cierta parte del código sin afectar más opciones (clases) o funciones(tareas)
 
 //La clase Empleados emplea un método llamado calculo Salario para calcular el salario de los empleados
 // pero dicho método ira modificando a la clase a medida se agreguen otras
+//Es decir que si se agrega asistente por ejemplo, debemos modificar y afectar por ende la función 'calculoSalario'
 class Empleados{
 
-    function calculoSalario($cargo){
-        if($cargo === "Gerente"){
+    function calculoSalario($cargo){//Se agrega una tarea que evalue el sueldo del empleado:
+        if($cargo === "Gerente"){//Sí es gerente, calculará salario
             //código de pago
-        }else if($cargo === "Diseñador"){
+        }else if($cargo === "Diseñador"){//Sí es diseñador
             //código de pago
-        }else if($cargo === "Administrador"){
+        }else if($cargo === "Administrador"){//Sí es administrador
             //código de pago
         }
     }
 }
 
-class Gerente{
+class Gerente{//Código para mostrar la información del gerente, el nuevo empleado que se ha agregado
     //Código
 }
 
-class Diseñador{
+class Diseñador{//Información del diseñador
     //Código
 }
 
-class Administrador{
+class Administrador{//Administrador
     //Código
 }
-?>
+
+//Pero si se agrega otro empleado, la función para calcular salario también se ira modificando 
