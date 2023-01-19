398e7797cf38324dac8310dbc99821674e9ae058
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2023-01-19 03:30:56 -0600

    Update Solución.php

--- SingleResponsibility/Solución.php
+++ SingleResponsibility/Solución.php
@@ -1,75 +1,78 @@
 <?php
 
-class Libreria{
+//Es así como en la solución vemos que se ha colocado diversas opciones dentro de la página
+//Estos pueden ser botones y cada uno posee responsabilidades especificas de acuerdo a su nombre
+
+class Libreria{ //Opción de libreria, lo que se muestra en la pág.principal
     protected $libros;
 
-    public function mostrarLibros(){
+    public function mostrarLibros(){//Tarea que se realiza al elegir dicha opción es mostrar los libros
         //código
     }
 }
 
-class Tipos{
+class Tipos{ //Al elegir el botón de tipos
     
-    public function impresos(){
+    public function impresos(){//Tarea de mostrar los impresos
         //código
     }
 
-    public function audiolibros(){
-        //código
-    }
-}
-
-class Categorías{
-
-    public function cocina(){
-        //código
-    }
-
-    public function educación(){
-        //código
-    }
-
-    public function autoayuda(){
-        //código
-    }
-
-    public function salud(){
-        //código
-    }
-
-    public function literatura(){
+    public function audiolibros(){//Tarea de mostrar los audiosLibros
         //código
     }
 }
 
-class obtenerLibros{
-    public function agregarLibros(){
+class Categorías{//Al elegir categorías
+
+    public function cocina(){//Tarea de mostrar los de tipo cocina
         //código
     }
 
-    public function quitarLibros(){
+    public function educación(){//Tarea de mostrar los de tipo educación
+        //código
+    }
+
+    public function autoayuda(){//Tarea de mostrar los de tipo autoayuda
+        //código
+    }
+
+    public function salud(){//Tarea de mostrar los de tipo salud
+        //código
+    }
+
+    public function literatura(){//Tarea de mostrar los de tipo literatura
         //código
     }
 }
 
-class Usuario{
-
-    public function logIn(){
+class obtenerLibros{ //Al elegir un botón para obtener el libro
+    public function agregarLibros(){//Agrega los libros 
         //código
     }
 
-    public function signUp(){
+    public function quitarLibros(){//Quita los libros escogidos
         //código
     }
 }
 
-class pagoLibros{
+class Usuario{//Al ingresar a nuestra cuenta
 
-    public function tarjetaDébito(){
+    public function logIn(){//Ingresaremos a una cuenta que hemos guardado
         //código
     }
 
-    public function tarjetaCrédito(){
+    public function signUp(){//Nos registrará por primera vez
+        //código
+    }
+}
+
+class pagoLibros{//Al elegir pagar los libros
+
+    public function tarjetaDébito(){//Realiza la tarea de pagar con tarjeta de débito
+        //código
+    }
+
+    public function tarjetaCrédito(){//Realiza la tarea de pagar con tarjeta de crédito
         //código
     }
 }
